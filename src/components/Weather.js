087d231e68549f3f2d3114518d1c5f19cc1c6ff3
Claudia Bringas-Everett <claudiaeverett9@gmail.com>
2020-04-27 15:33:31 -0400

    made stateless weather.js and form.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const Weather = () => {
-    return (
-        <div>
-            {this.props.city && this.props.region && this.props.country && <p>Location: {this.props.city}, {this.props.region}, {this.props.country}</p>}
-            {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-            {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-            {this.props.text && <p>Conditions: {this.props.text}</p>}
-            {this.props.error && <p>{this.props.error}</p>}
-        </div>
-    );
-}
+const Weather = props => (
+<div>
+    {props.city && props.region && props.country && <p>Location: {props.city}, {props.region}, {props.country}</p>}
+    {props.temperature && <p>Temperature: {props.temperature}</p>}
+    {props.humidity && <p>Humidity: {props.humidity}</p>}
+    {props.text && <p>Conditions: {props.text}</p>}
+    {props.error && <p>{props.error}</p>}
+</div>
+
+);
 
 export default Weather;
